2e87861c7d2c1b24cfea0bbbc770a6082e64b85a
Danh <danhchutich@gmail.com>
2016-05-09 17:03:47 +0700

    Fix error on dashboard page 9/5/2015

--- admin/widget/toper_dashboard.php
+++ admin/widget/toper_dashboard.php
@@ -1,9 +1,10 @@
-<div class="page-header">
+<div id="content">
+  <div class="page-header">
     <div class="container-fluid">
       <h1>Dashboard</h1>
       <ul class="breadcrumb">
-                <li><a href="http://localhost/opencart/upload/admin/index.php?route=common/dashboard&amp;token=qC5k8ZOYbcCfczQ5PfnFBWZ6umTOjt2u">Home</a></li>
-                <li><a href="http://localhost/opencart/upload/admin/index.php?route=common/dashboard&amp;token=qC5k8ZOYbcCfczQ5PfnFBWZ6umTOjt2u">Dashboard</a></li>
+                <li><a href="http://localhost/opencart/upload/admin/index.php?route=common/dashboard&amp;token=o9dInnw2E1FkkesNiP9eooxV50HVO4On">Home</a></li>
+                <li><a href="http://localhost/opencart/upload/admin/index.php?route=common/dashboard&amp;token=o9dInnw2E1FkkesNiP9eooxV50HVO4On">Dashboard</a></li>
               </ul>
     </div>
   </div>
